b736ebe63cec3345f60e35bc76438ce7b02e0b70
Alexandre Pauwels <8890201+ajpauwels@users.noreply.github.com>
2021-02-03 12:15:10 +0000

    fix: merge homepage code in

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,110 @@
-#![feature(plugin)]
-#![plugin(rocket_codegen)]
+use handlebars::Handlebars;
+use reqwest::Client;
+use rust_config::Configurator;
+use serde::Serialize;
+use serde_json::json;
+use std::collections::HashMap;
+use std::sync::Arc;
+use warp::Filter;
 
-
-extern crate rocket;
-
-use std::io;
-use std::path::{Path, PathBuf};
-
-use rocket::response::NamedFile;
-
-#[get("/")]
-fn index() -> io::Result<NamedFile> {
-    NamedFile::open("static/index.html")
+struct WithTemplate<T: Serialize> {
+    name: &'static str,
+    value: T,
 }
 
-#[get("/<file..>")]
-fn files(file: PathBuf) -> Option<NamedFile> {
-    NamedFile::open(Path::new("static/").join(file)).ok()
+fn render<T>(template: WithTemplate<T>, hbs: Arc<Handlebars>) -> impl warp::Reply
+where
+    T: Serialize,
+{
+    let render = hbs
+        .render(template.name, &template.value)
+        .unwrap_or_else(|err| err.to_string());
+    warp::reply::html(render)
 }
 
-fn rocket() -> rocket::Rocket {
-    rocket::ignite().mount("/", routes![index, files])
-}
+#[tokio::main]
+async fn main() {
+    let config = rust_config::new("WEBSITE").unwrap();
 
-fn main() {
-    rocket().launch();
+    let mut hb = Handlebars::new();
+    hb.register_template_file("index", "./static/index.html")
+        .unwrap();
+
+    let hb = Arc::new(hb);
+
+    let handlebars = move |with_template| render(with_template, hb.clone());
+
+    let index_route = warp::path::end()
+        .map(|| WithTemplate {
+            name: "index",
+            value: json!({}),
+        })
+        .map(handlebars.clone());
+    let expandable_route =
+        warp::path!("expandable").and(warp::fs::file("./static/expandable.html"));
+    let scalable_route = warp::path!("scalable").and(warp::fs::file("./static/scalable.html"));
+    let highly_available_route =
+        warp::path!("highly-available").and(warp::fs::file("./static/highly-available.html"));
+    let full_stack_route =
+        warp::path!("full-stack").and(warp::fs::file("./static/full-stack.html"));
+    let full_service_route =
+        warp::path!("full-service").and(warp::fs::file("./static/full-service.html"));
+    let cloud_route = warp::path!("cloud").and(warp::fs::file("./static/cloud.html"));
+    let css_routes = warp::path!("css" / ..).and(warp::fs::dir("./static/css"));
+
+    let slack_webhook_url = config.get_str("slack.webhook").unwrap();
+    let message_route = warp::path!("message")
+        .and(warp::post())
+        .and(warp::body::content_length_limit(1024 * 20))
+        .and(warp::body::form())
+        .map(|form_map: HashMap<String, String>| {
+            form_map
+                .get("message")
+                .map(|m| m.to_string())
+                .unwrap_or("".to_string())
+        })
+        .and_then(move |msg: String| {
+            let slack_webhook_url = slack_webhook_url.clone();
+            async move {
+                if msg.len() <= 0 {
+                    return Ok::<_, std::convert::Infallible>(WithTemplate {
+                        name: "index",
+                        value: json!({ "msg": "", "error-msg": "Don't forget to write a message"}),
+                    });
+                }
+
+                let mut slack_body_map = HashMap::new();
+                slack_body_map.insert("text", format!(r#"<!channel> {}"#, msg));
+
+                Client::new()
+                    .post(&slack_webhook_url)
+                    .json(&slack_body_map)
+                    .send()
+                    .await
+    		    .map_or_else(|_| {
+    			Ok(WithTemplate {
+    			    name: "index",
+    			    value: json!({ "msg": msg, "error-msg": "An error occurred while sending, try again in a little bit" }),
+    			})
+    		    }, |_| {
+    			Ok(WithTemplate {
+    			    name: "index",
+    			    value: json!({ "success-msg": "Message received, expect a response within 24 hours" }),
+    			})
+    		    })
+            }
+        })
+        .map(handlebars.clone());
+
+    let static_routes = index_route
+        .or(expandable_route)
+        .or(scalable_route)
+        .or(highly_available_route)
+        .or(full_stack_route)
+        .or(full_service_route)
+        .or(cloud_route)
+        .or(css_routes)
+        .or(message_route);
+
+    warp::serve(static_routes).run(([0, 0, 0, 0], 8080)).await;
 }
